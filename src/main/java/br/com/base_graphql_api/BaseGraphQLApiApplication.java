9268940435cc4c835551d768472e40d0a6333e74
wmendes-dev <whdesenvolvedor@gmail.com>
2025-06-14 00:26:29 -0400

    Renomeando classe Application para padronizar com os demais projetos

--- src/main/java/br/com/base_graphql_api/BaseGraphQLApiApplication.java
+++ src/main/java/br/com/base_graphql_api/BaseGraphQLApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaseGraphQLApplication {
+public class BaseGraphQLApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BaseGraphQLApplication.class, args);
+        SpringApplication.run(BaseGraphQLApiApplication.class, args);
     }
 
 }
